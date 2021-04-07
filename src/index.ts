69f5a5239d7d32c18b3bf1b51b92b1b3b926d320
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-04-07 01:54:03 -0600

    added starwars api

--- src/index.ts
+++ src/index.ts
@@ -2,115 +2,83 @@ import * as _ from 'lodash'
 import './pages/styles.css'
 
 
+//const response = await fetch('https://swapi.dev/api/people/1/')
 
 
-window.addEventListener('load', () => {
-
-fetch ('https://pokeapi.co/api/v2/pokemon')
+fetch ('https://swapi.dev/api/')
 .then((mydata => mydata.json()))
 .then((mydata) => {
     console.log(mydata)
-    console.log(mydata.results[0].name)
+    console.log(mydata.people[0].name)
     
 
-
-
-
-
-    const header = document.createElement('h1')
-    header.innerText = "Pokemon" 
-    const header2 = document.createElement('h2')
-    header2.innerText = "Collect Them All!"
-    const body = document.querySelector('body')  
-   
-    body.appendChild(header)
-    body.appendChild(header2)
-   
-
+interface Character {
+    id: 0;
+    name: string; 
+    hair_color: string; 
+    height: number;
     
-    const pokemon = [
-        {
-            id: 0, 
-            name: mydata.results[0].name, 
-            image: `${mydata.results[0].sprites}`, 
-            url: mydata.results[0].url
-        }, 
-        {
-            id: 1, 
-            name: mydata.results[1].name,
-            image: `${mydata.results[1].sprites}`,  
-            url: mydata.results[1].url
-        },
-        {
-            id: 2, 
-            name: mydata.results[2].name,
-            image: `${mydata.results[2].sprites}`,  
-            url: mydata.results[2].url
-        },
-        {
-            id: 3, 
-            name: mydata.results[3].name, 
-            image: `${mydata.results[3].sprites}`,  
-            url: mydata.results[3].url
-        },
-    ]; 
-
-    const listItem1 = document.createElement('li')
-    listItem1.innerText = _.find(pokemon, {id: 0})?.name;
-
-    const button1 = document.createElement('button')
-    button1.innerText = "MORE INFORMATION"; 
-    button1.addEventListener("Click", function() {
-        document.getElementById('button').innerHTML = mydata.results[0].url
-    } )
-
-    const img1 = document.createElement('img')
-    img1.src = pokemon[0].image
-    
-    const listItem2 = document.createElement('li')
-    listItem2.innerText = _.find(pokemon, {id: 1})?.name; 
-    
-
-    const button2 = document.createElement('button')
-    button2.innerText = "MORE INFORMATION"; 
-    
-
-    const listItem3 = document.createElement('li')
-    listItem3.innerText = _.find(pokemon, {id: 2})?.name; 
-    
-
-    
-    const button3 = document.createElement('button')
-    button3.innerText = "MORE INFORMATION"; 
-    
-
-    const listItem4 = document.createElement('li')
-    listItem4.innerText = _.find(pokemon, {id: 3})?.name; 
-    
-
-    
-    const button4 = document.createElement('button')
-    button4.innerText = "MORE INFORMATION"; 
-    
+}
 
 
-    body.appendChild(listItem1); 
-    body.appendChild(button1); 
-    body.appendChild(img1)
-    body.appendChild(listItem2);
-    body.appendChild(button2);
-    body.appendChild(listItem3); 
-    body.appendChild(button3);
-    body.appendChild(listItem4); 
-    body.appendChild(button4);
+function CharacterName(character: Character) {
+    return "Hello" + character.name; 
+}
 
 
-   
- 
+const body = document.querySelector('body')
+const header1 = document.createElement('h1')
+header1.innerText = _.find(Character, {id: 0})?.name;
+body.appendChild(header1)
+const character = document.createElement('h2')
+character.innerText = _.find(Character, {id: 0})?.name;
+body.appendChild(character)
+const names = document.createElement('div')
 
 
 
+interface Person {
+    name: string; 
+    age: number; 
+}
 
 
-})
-})
+function greet(person: Person) {
+    return "Hello" + person.age; 
+}
+
+const greetingText = document.createElement('div') 
+greetingText.innerText = _.find(greet)
+body.appendChild(greetingText)
+
+
+type PersonB = {
+    name: string; 
+    age: number; 
+}
+
+
+function greet2(person: PersonB) {
+    return "Hello" + person.age; 
+}
+
+
+type ResponseA = {
+    welcomeMessage: "Welcome to my Star Wars Page." 
+}
+
+
+
+function message1(message: ResponseA) {
+    return message.welcomeMessage; 
+}
+
+
+type ResponseB = {
+    welcomeMessage2: "Feel free to get to know the characters." 
+}
+
+
+function message2(message: ResponseB) {
+    return message.welcomeMessage2; 
+}
